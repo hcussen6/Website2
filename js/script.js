a9dfcc62154cc99ddccc14de2ac64297faa3beca
hcussen6 <github@hannahcussen.com>
2020-04-02 11:59:06 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+/*global document, jQuery*/
+/*jslint strict: false */
+
+$(document).ready(function () {
+  $('#get-started').on('click', function () {
+    $('#hidden-text').show();
+  });
+});
+
